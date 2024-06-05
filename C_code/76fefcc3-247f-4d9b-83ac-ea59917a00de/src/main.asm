0fa81d34f66b0bb334fdfaa84df201f413db39b2
cpmores <chaoran550@gmail.com>
2024-06-05 21:44:02 +0800

    masm update

--- C_code/76fefcc3-247f-4d9b-83ac-ea59917a00de/src/main.asm
+++ C_code/76fefcc3-247f-4d9b-83ac-ea59917a00de/src/main.asm
@@ -81,52 +81,131 @@ main:
 ;    所有补充代码包括自定义函数及变量均必须
 ;    放在#1_begin和#1_end之间
 ;#1_begin-------------------------------------
-   mov ecx, 0 ;store loading number
-   mov edx, 0 ;store result now
 check:
-   dec bl
-   jz result
-
+   xor edx, edx
+   xor ebx, ebx
+   xor eax, eax
+   xor ecx, ecx
    lodsb
-   cmp al, '0'
-   jb operator
+   cmp ax, '0'
+   jb op_first
+   sub eax, '0'
+   mov ebx, eax 
+   mov eax, [c]
+   mov ecx, 0Ah
+   mul ecx
+   add eax, ebx
+   mov [c], eax
+   jmp check
 
+op_first:
+   cmp ax, '+'
+   je add_op
+   cmp ax, '*'
+   je mul_op
+   cmp ax, '/'
+   je div_op
+   cmp ax, 00h
+   je last_operator
+
+add_op:
+   mov ax, 1
+   jmp operator
+mul_op:
+   mov ax, 2
+   jmp operator
+div_op:
+   mov ax, 3
+   jmp operator
 
 operator:
    cmp prev_op, 1
-   je add_op
+   je fun_add
    cmp prev_op, 2
-   je mul_op
+   je fun_mul
    cmp prev_op, 3
-   je div_op
+   je fun_div
    jmp result
 
-add_op:
-   mov prev_op, al
-   add edx, ecx
-   xor ecx, ecx
-   jmp check
+last_operator:
+   cmp prev_op, 1
+   je fun_add_last
+   cmp prev_op, 2
+   je fun_mul_last
+   cmp prev_op, 3
+   je fun_div_last
+   jmp result
 
-mul_op:
-   mov prev_op, al
-   mov eax, edx
+fun_add:
+   mov prev_op, ax
+   mov ecx, [c]
+   mov eax, [b]
+   add eax, ecx
+   mov [b], eax
+   jmp clear
+
+fun_mul:
+   mov prev_op, ax
+   mov ecx, [c]
+   mov eax, [b] 
    mul ecx
-   mov edx, eax
+   mov [b], eax
+   jmp clear
+
+fun_div:
+   mov prev_op, ax
+   mov ecx, [c]
+   mov eax, [b] 
+   xor edx, edx
+   div ecx
+   mov [b], eax
+   jmp clear
+
+clear:
+   xor eax, eax
+   xor ebx, ebx
    xor ecx, ecx
+   xor edx, edx
+   mov [c], ecx
    jmp check
 
-div_op:
-   mov prev_op, al
-   mov eax, edx
+fun_add_last:
+   mov prev_op, ax
+   mov ecx, [c]
+   mov eax, [b]
+   add eax, ecx
+   mov [b], eax
+   jmp clear_last
+
+fun_mul_last:
+   mov prev_op, ax
+   mov ecx, [c]
+   mov eax, [b] 
+   mul ecx
+   mov [b], eax
+   jmp clear_last
+
+fun_div_last:
+   mov prev_op, ax
+   mov ecx, [c]
+   mov eax, [b]
+   xor edx, edx 
    div ecx
-   and eax, 0000FFFFh
-   mov edx, eax
+   mov [b], eax
+   jmp clear_last
+
+clear_last:
+   xor eax, eax
+   xor ebx, ebx
    xor ecx, ecx
-   jmp check   
+   xor edx, edx
+   mov [c], edx
+   jmp result
+
 
 result:
-   mov [b], edx
    jmp do_output
+
 ;#1_end=======m================================
 do_output:
    mov eax, [b]
